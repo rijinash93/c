6492713b3af84026b8b5a7908a688f05a335059f
rijinash93 <rijinash93@gmail.com>
2022-02-18 21:25:01 +0530

    first commit

---
+++ security.py
@@ -0,0 +1,43 @@
+import cv2
+import dropbox
+import time
+import random
+
+start_time = time.time()
+
+def take_snapshot():
+    number = random.randint(0,100)
+    #initializing cv2
+    videoCaptureObject = cv2.VideoCapture(0)
+    result = True
+    while(result):
+        #read the frames while the camera is on
+        ret,frame = videoCaptureObject.read()
+        #cv2.imwrite() method is used to save an image to any storage device
+        img_name = "img"+str(number)+".png"
+        cv2.imwrite(img_name, frame)
+        start_time = time.time
+        result = False
+    return img_name
+  
+
+
+def upload_file(img_name):
+    access_token = "rQ4A2gU9GUgAAAAAAAAAAac4IUbxcR5YW5U79-0MEbf1fmYn3YrlcBLSQecow6jk"
+    file =img_name
+    file_from = file
+    file_to="/testFolder/"+(img_name)
+    dbx = dropbox.Dropbox(access_token)
+
+    with open(file_from, 'rb') as f:
+        dbx.files_upload(f.read(), file_to,mode=dropbox.files.WriteMode.overwrite)
+        print("file uploaded")
+
+
+def main():
+    while(True):
+        if ((time.time() - start_time) >= 5):
+            name = take_snapshot()
+            upload_file(name)
+
+main()
